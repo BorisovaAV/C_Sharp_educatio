eb5d497e0d4c37bd652b3c8f213b59ae6661266f
Anna Borisova <av.borisova@bk.ru>
2023-01-06 03:11:39 +0500

    add Example010_MethodArray

---
+++ Examples010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1, 12, 31, 10, 15, 10, 17, 18 };
+
+int n=array.Length;
+int find = 10;
+
+int index = 0;
+
+while (index <n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
